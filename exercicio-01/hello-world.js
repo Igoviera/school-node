952c957b3b7cf510e582546a94980efeafe179ee
Igo vieira <igovieira87@gmail.com>
2023-03-15 11:36:36 -0300

    Primeiro commit

---
+++ exercicio-01/hello-world.js
@@ -0,0 +1,6 @@
+console.log('Meu primeiro arquivo javascript no node!')
+
+const n1 = 2
+const n2 = 2
+
+console.log(`O resultado da soma de 2 + 2: ${n1 + n2}`)
---
+++ primeiro-projeto/primeiro-arquivo.ts
@@ -0,0 +1,8 @@
+console.log('Primeiro arquivo typescript');
+
+let numero1: number = 5;
+let numero2: number = 10;
+
+const texto: string = 'Texto 2'
+
+console.log(numero1 + numero1);
